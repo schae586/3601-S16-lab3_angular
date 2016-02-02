13f0e78f819e11edeaceac22e7284f8bc290fc1e
schae586 <schae586@morris.umn.edu>
2016-02-02 14:48:42 -0500

    Added stuff for a footer module and footer.html and other necessary footer Angular stuff.

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -4,9 +4,9 @@
 // angular.module('nameOfModule', ['dependency1', 'dependency2', 'dependency3']);
 // Dependencies will be automatically loaded for the modules that list them as requirements.
 
+angular.module("stdComponents", [])
 angular.module('stdControllers', []);
-angular.module("mainApp", ['stdControllers']);
-
+angular.module("mainApp", ['stdControllers','stdComponents']);
 
 
 
---
+++ public/javascript/footer.controller.js
@@ -0,0 +1,4 @@
+angular.module('stdComponents').controller('footerCtrl', function() {
+    var footer = this;
+    console.log("Navbar controller loaded");
+});
